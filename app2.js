0fd39f11580db27f87887d52c7dc7ead732be6d4
Matias Vinković <matnum4@gmail.com>
2022-08-16 01:29:58 +0200

    new page working on

---
+++ app2.js
@@ -0,0 +1,12 @@
+
+
+//chargement de la page HTML2
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+
+    loader.classList.add('fondu-out');
+    loader.style.zIndex = "0";
+
+})
+
